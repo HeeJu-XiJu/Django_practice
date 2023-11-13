5d4369cc76199794fe66926c119458c24e9cd58a
hhjjoa2817 <hhjjoa2817@naver.com>
2023-11-13 23:14:17 +0900

    Post, Comment, User 모델링/마이그레이션

--- auth review2/README.md
+++ auth review2/README.md
@@ -1,3 +1,12 @@
 1. 프로젝트 생성
 2. 앱생성/앱등록
 3. 'base.html' 설정
+4. Post, Comment, User 모델링/마이그레이션
+Post : Comment = 1 : N
+User : Post = 1 : N
+Post : Comment = 1 : N
+
+AbstractUser 기본 모델
+![AbstracUser 기본모델](/auth%20review2/posts/reference/image.png)
+
+AbstracUser을 불러오는 방법 : get_user_model
--- auth review2/posts/models.py
+++ auth review2/posts/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+
+class Comment(models.Model):
+    comment = models.TextField()
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
